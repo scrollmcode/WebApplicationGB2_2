ed81eedeab9c7e126163f25ddee3a5234516b867
ScrollmCode <Supermcode@mail.ru>
2021-08-06 21:09:42 +0300

    Добавьте файлы проекта.

---
+++ WebApplicationGB2_2/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationGB2_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // /api/example/index
+        public IActionResult Index()
+        {
+            return Content("Проверка работы контроллера.");
+        }
+
+        [HttpGet]
+        public IEnumerable<int> GetNumbers()
+        {
+            return Enumerable.Range(1, 100);
+        }
+    }
+}
